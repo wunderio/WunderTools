c175029b17f923ca131c83d29e602ea669de36ad
Teemu Merikoski <teemu.merikoski@gmail.com>
2015-02-24 09:29:46 +0200

    Aliases file which tries to be as generic as possible

---
+++ drupal/conf/aliases.drushrc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Solve the key file to use
+$path = explode('/', dirname(__FILE__));
+array_pop($path);
+array_pop($path);
+$path[] = '.vagrant';
+$path = implode('/', $path);
+$key = shell_exec('find ' . $path . ' -iname private_key');
+if (!$key) {
+  $key = '~/.vagrant.d/insecure_private_key';
+}
+$key = rtrim($key);
+
+$aliases['vagrant'] = array(
+  'parent' => '@parent',
+  'site' => 'ansibleref',
+  'env' => 'vagrant',
+  'root' => '/vagrant/drupal/current',
+  'remote-host' => 'local.ansibleref.com',
+  'remote-user' => 'vagrant',
+  'ssh-options' => '-i ' . $key,
+);
