00847f18772fd641be1d2e1439410cf5ee6bc196
Janne Koponen <janne.koponen@wunderkraut.com>
2015-10-12 15:51:54 +0300

    Improved build.sh
    - added reset command
    - added drush alias file check and linking

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Helper function to parse yaml configs
 function parse_yaml {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
@@ -16,28 +17,53 @@ function parse_yaml {
    }'
 }
 
-if [[ $1 == 'up' || $1 == 'provision' ]]; then
-  path="`pwd`"
+SCRIPT=$(readlink -f $0)
+ROOT=`dirname $SCRIPT`
+# Parse project config
+PROJECTCONF=$ROOT/conf/project.yml
+eval $(parse_yaml $PROJECTCONF)
+ALIASFILE=${project_name}.aliases.drushrc.php
+ALIASPATH=$ROOT/drupal/conf/$ALIASFILE
+ALIASTARGET=$HOME/.drush/$ALIASFILE
 
-  eval $(parse_yaml conf/project.yml)
+if [[ $1 == "reset" ]]; then
+  read -p "This will reset everything! Are you sure?" -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    cd $PATH
+    vagrant destroy
+    rm -r $PATH/ansible
+    rm $ALIASTARGET
+  fi
+# Only run when running vagrant up or provision
+elif [[ $1 == "up" || $1 == "provision" ]]; then
+  # Clone and update virtual environment configurations
   if [ ! -d "ansible" ]; then
     git clone  -b $ansible_branch $ansible_remote ansible
     if [ -n "$ansible_revision" ]; then
       cd ansible
       git reset --hard $ANSIBLE_revision
-      cd $path
+      cd $ROOT
     fi
   else
     if [ -z "$ansible_revision" ]; then
       cd ansible
       git pull
-      cd $path
+      cd $ROOT
     fi
   fi
+
+  # Get & update drupal/build.sh
   if [ -n "$buildsh_revision" ]; then
     curl -o drupal/build.sh https://raw.githubusercontent.com/wunderkraut/build.sh/$buildsh_revision/build.sh
   else
     curl -o drupal/build.sh https://raw.githubusercontent.com/wunderkraut/build.sh/$buildsh_branch/build.sh
+  fi
+
+  # Ensure drush aliases file is linked
+  if [ ! -h $ALIASTARGET ] || [ ! "$(readlink $ALIASTARGET)" -ef "$ALIASPATH" ]; then
+    rm $ALIASTARGET
+    ln -s $ALIASPATH $ALIASTARGET
   fi
 fi
 
