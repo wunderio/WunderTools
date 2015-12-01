48a61b18cfb04f8895c3bd08ace783d756b574de
Janne Koponen <janne.koponen@wunderkraut.com>
2015-12-01 15:11:38 +0200

    Cleanup FeatureContext.

--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -8,15 +8,7 @@ use Behat\Gherkin\Node\TableNode;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends Behat\MinkExtension\Context\MinkContext {
-
-  /**
-   * @BeforeSuite
-   */
-  public static function setupSuite(SuiteEvent $event) {
-    $behatContext = new BehatContext();
-    $behatContext->printDebug('Context parameters: ' . json_encode($event->getContextParameters()));
-  }
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
    * Initializes context.
@@ -28,17 +20,4 @@ class FeatureContext extends Behat\MinkExtension\Context\MinkContext {
   public function __construct() {
   }
 
-  /**
-   * Custom step definition. Step definitions are mapped to matching PHP
-   * functions using regular expressions. Step definitions can have input
-   * placeholders.
-   *
-   * @When /^I search for "([^"]*)"$/
-   */
-  public function iSearchFor($text) {
-    return array(
-      new Step\When('I fill in "edit-search-api-views-fulltext" with "'.$text.'"'),
-      new Step\When('I press "edit-submit-ns-prod-enterprise-search"'),
-    );
-  }
 }
