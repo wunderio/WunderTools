cf4447dd0b1c434c26101e1799860c81ca95ecf9
federico prato <federico.prato@wunderkraut.com>
2016-01-05 12:31:45 +0200

    Added the snippets generation thingie and a couple simple steps on the login scenario

--- test/features/00-login.feature
+++ test/features/00-login.feature
@@ -7,4 +7,15 @@ Feature: Javascript-enabled test
     And   I fill in "pass" with "admin"
     And   I press "edit-submit"
     Then  I should not see "Sorry, unrecognized username or password. Have you forgotten your password?"
-    And  I should see "admin"
+    And   I should see "admin"
+
+  Scenario: Logout
+    Given I go to "user/logout"
+    Then  I should find "body.not-logged-in"
+
+  Scenario: The login fails with a sensible message with wrong username
+    Given I go to "user"
+    Given I fill in "name" with "1234"
+    And   I fill in "pass" with "admin"
+    And   I press "edit-submit"
+    Then  I should see "Sorry, unrecognized username or password. Have you forgotten your password?"
--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -1,10 +1,13 @@
 <?php
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Hook\Scope\AfterStepScope;
 use Behat\Mink\Driver\Selenium2Driver;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends Behat\MinkExtension\Context\MinkContext {
+class FeatureContext extends Behat\MinkExtension\Context\MinkContext implements SnippetAcceptingContext{
     /**
    * Initializes context.
    *
@@ -18,17 +21,27 @@ class FeatureContext extends Behat\MinkExtension\Context\MinkContext {
   }
 
   /**
-     * Take a screenshot and save html when step fails. Works only with Selenium2Driver.
-     *
-     * @AfterStep
-     * @param AfterStepScope $scope
-     */
-    public function takeScreenshotAfterFailedStep(Behat\Behat\Hook\Scope\AfterStepScope $scope)
-    {
-      if (99 === $scope->getTestResult()->getResultCode()) {
-        $this->debugger->getScreenshot($scope);
-        $this->debugger->saveHtml($scope);
-      }
+   * Take a screenshot and save html when step fails. Works only with Selenium2Driver.
+   *
+   * @AfterStep
+   * @param AfterStepScope $scope
+   */
+  public function takeScreenshotAfterFailedStep(Behat\Behat\Hook\Scope\AfterStepScope $scope)
+  {
+    if (99 === $scope->getTestResult()->getResultCode()) {
+      $this->debugger->getScreenshot($scope);
+      $this->debugger->saveHtml($scope);
     }
+  }
 
+  /**
+  * CUSTOM IMPLEMENTATIONS
+  */
+  /**
+   * @Then I should find :arg1
+   */
+  public function iShouldFind($arg1)
+  {
+      return $this->getSession()->getPage()->find('css',$arg1);
+  }
 }
