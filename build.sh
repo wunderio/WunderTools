e65b62ca904dd6c85cd0f8af136fa8dce32bc234
Janne Koponen <janne.koponen@wunderkraut.com>
2016-04-05 10:34:51 +0300

    Update build.sh to support changelog.

--- build.sh
+++ build.sh
@@ -17,6 +17,10 @@ function parse_yaml {
    }'
 }
 
+# Remember to update this on each release
+# Also update the changelog!
+VERSION=1
+
 pushd `dirname $0` > /dev/null
 ROOT=`pwd -P`
 popd > /dev/null
@@ -27,6 +31,30 @@ ALIASFILE=${project_name}.aliases.drushrc.php
 ALIASPATH=$ROOT/drupal/conf/$ALIASFILE
 ALIASTARGET=$HOME/.drush/$ALIASFILE
 
+GITBRANCH=`git symbolic-ref --short -q HEAD`
+
+VERSIONFILE=$ROOT/VERSION
+CHANGELOG=$ROOT/CHANGELOG
+if [ -f $VERSIONFILE ]; then
+  typeset -i CURRENT_VERSION=$(<$VERSIONFILE)
+else
+  echo $VERSION > $VERSIONFILE
+  CURRENT_VERSION=0
+fi
+
+if [ "$CURRENT_VERSION" -ne "$VERSION" ]; then
+  echo "Build.sh version has been updated. Make sure your project complies with the changes outlined in the CHANGELOG since version $CURRENT_VERSION"
+  while read -p "I have updated everything ([y]es / [n]o / show [c]hangelog)? " -n 1 -r && [[ $REPLY =~ ^[Cc]$ ]]; do
+    sed -e '/^'$CURRENT_VERSION'$/,$d' $CHANGELOG
+  done
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo $VERSION > $VERSIONFILE
+    echo -e "\nCurrent version updated, make sure to commit all the changes before continuing."
+  fi
+fi
+
+exit
+
 if command -v md5sum >/dev/null 2>&1; then
   MD5COMMAND="md5sum"
 else
@@ -34,7 +62,7 @@ else
 fi
 
 if [[ $1 == "reset" ]]; then
-  read -p "This will reset everything! Are you sure?" -n 1 -r
+  read -p "This will reset everything! Are you sure (y/n)? " -n 1 -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     cd $ROOT
@@ -46,16 +74,21 @@ if [[ $1 == "reset" ]]; then
 elif [[ $1 == "up" || $1 == "provision" ]]; then
   # First we check if there is update for this script
   SELF=$(basename $0)
-  UPDATEURL="https://raw.githubusercontent.com/wunderkraut/WunderTools/master/build.sh"
+  UPDATEURL="https://raw.githubusercontent.com/wunderkraut/WunderTools/$GITBRANCH/build.sh"
+  CHANGELOGURL="https://raw.githubusercontent.com/wunderkraut/WunderTools/$GITBRANCH/CHANGELOG"
   MD5SELF=$($MD5COMMAND $0 | awk '{print $1}')
   MD5LATEST=$(curl -s $UPDATEURL | $MD5COMMAND | awk '{print $1}')
   if [[ "$MD5SELF" != "$MD5LATEST" ]]; then
-    read -p "There is update for this script available. Update now?" -n 1 -r
+    while read -p "There is update for this script available. Update now ([y]es / [n]o / show [c]hangelog)?" -n 1 -r && [[ $REPLY =~ ^[Cc]$ ]]; do
+      curl -s $CHANGELOGURL | sed -e '/^'$CURRENT_VERSION'$/,$d'
+    done
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       cd $ROOT
       curl -o $SELF $UPDATEURL
+      curl -o $CHANGELOG $CHANGELOGURL
       echo "Update complete, please rerun any command you were running previously."
+      echo "See CHANGELOG for more info."
       echo "Also remember to add updated script to git."
       exit
     fi
