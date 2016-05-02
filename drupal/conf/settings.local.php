0e13d2ea8b5a89bf184fc6126e5025358b1ef864
Marc Galang <marc.galang@wunderkraut.com>
2016-05-02 14:30:42 +0300

    Add settings.local.php and default.settings.php

---
+++ drupal/conf/settings.local.php
@@ -0,0 +1,52 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'password',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['hash_salt'] = 'some-hash-salt-please-change-this';
+
+$settings['trusted_host_patterns'] = array(
+  '^local\.ansibleref\.com',
+);
+
+if ( (isset($_SERVER["HTTPS"]) && strtolower($_SERVER["HTTPS"]) == "on")
+  || (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] == "https")
+  || (isset($_SERVER["HTTP_HTTPS"]) && $_SERVER["HTTP_HTTPS"] == "on")
+) {
+  $_SERVER["HTTPS"] = "on";
+
+  // Tell Drupal we're using HTTPS (url() for one depends on this).
+  $conf['https'] = TRUE;
+}
+
+if (isset($_SERVER['REMOTE_ADDR'])) {
+  $settings['reverse_proxy'] = TRUE;
+  $settings['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
+}
+
+/**
+ * Show all error messages, with backtrace information.
+ *
+ * In case the error level could not be fetched from the database, as for
+ * example the database connection failed, we rely only on this value.
+ */
+$config['system.logging']['error_level'] = 'verbose';
+
+/**
+ * Disable CSS and JS aggregation.
+ */
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+/*
+ * Set configuration directy
+ */
+$config_directories['staging'] = '../staging';
