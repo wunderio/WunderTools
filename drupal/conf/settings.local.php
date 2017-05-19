a966f4b4f63c5ea095ff48e98c7da0e49d647aa5
Janne Koponen <janne.koponen@wunder.io>
2017-05-19 12:31:34 +0300

    Move common settings to settings.php, use env vars for db credentials.

--- drupal/conf/settings.local.php
+++ drupal/conf/settings.local.php
@@ -4,35 +4,6 @@
  * This should be overridden in staging and production environments.
  */
 
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'password',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-
-// CHANGE THIS.
-$settings['hash_salt'] = 'some-hash-salt-please-change-this';
-
-if ( (isset($_SERVER["HTTPS"]) && strtolower($_SERVER["HTTPS"]) == "on")
-  || (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] == "https")
-  || (isset($_SERVER["HTTP_HTTPS"]) && $_SERVER["HTTP_HTTPS"] == "on")
-) {
-  $_SERVER["HTTPS"] = "on";
-
-  // Tell Drupal we're using HTTPS (url() for one depends on this).
-  $conf['https'] = TRUE;
-}
-
-if (isset($_SERVER['REMOTE_ADDR'])) {
-  $settings['reverse_proxy'] = TRUE;
-  $settings['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
-}
-
 /**
  * Assertions.
  *
