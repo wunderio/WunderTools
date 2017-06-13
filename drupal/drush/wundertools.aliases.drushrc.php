9a73cdbf949fdf752e0de6babe9da55c4e3c5cc6
Onni Hakala <onni@keksi.io>
2017-06-13 18:03:51 +0300

    Fix error notices from drush aliases file in non-local environments

--- drupal/drush/wundertools.aliases.drushrc.php
+++ drupal/drush/wundertools.aliases.drushrc.php
@@ -6,11 +6,18 @@ array_pop($path);
 array_pop($path);
 $path[] = '.vagrant';
 $path = implode('/', $path);
-$key = shell_exec('find ' . $path . ' -iname private_key');
-if (!$key) {
-  $key = $home . '/.vagrant.d/insecure_private_key';
+
+// If the .vagrant folder exists find the ssh key for the virtual machine
+if ( file_exists($path) ) {
+  $key = shell_exec('find ' . $path . ' -iname private_key');
+  if (!$key) {
+    $key = $home . '/.vagrant.d/insecure_private_key';
+  }
+  $key = rtrim($key);
+} else {
+  // .vagrant directory doesn't exist, just use empty key
+  $key = "";
 }
-$key = rtrim($key);
 
 $aliases['local'] = array(
   'parent' => '@parent',
