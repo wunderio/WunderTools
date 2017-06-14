2924ef4d56577bbcdddfe2b13b97e1b9f08b2d1d
Janne Koponen <janne.koponen@wunder.io>
2017-06-14 09:31:58 +0300

    Only check vagrant key on local environment in drush aliases.

--- drupal/drush/wundertools.aliases.drushrc.php
+++ drupal/drush/wundertools.aliases.drushrc.php
@@ -1,16 +1,18 @@
 <?php
-$home = drush_server_home();
-// Solve the key file to use
-$path = explode('/', dirname(__FILE__));
-array_pop($path);
-array_pop($path);
-$path[] = '.vagrant';
-$path = implode('/', $path);
-$key = shell_exec('find ' . $path . ' -iname private_key');
-if (!$key) {
-  $key = $home . '/.vagrant.d/insecure_private_key';
+if (getenv('WKV_SITE_ENV' == 'local')) {
+  $home = drush_server_home();
+  // Solve the key file to use
+  $path = explode('/', dirname(__FILE__));
+  array_pop($path);
+  array_pop($path);
+  $path[] = '.vagrant';
+  $path = implode('/', $path);
+  $key = shell_exec('find ' . $path . ' -iname private_key');
+  if (!$key) {
+    $key = $home . '/.vagrant.d/insecure_private_key';
+  }
+  $key = rtrim($key);
 }
-$key = rtrim($key);
 
 $aliases['local'] = array(
   'parent' => '@parent',
