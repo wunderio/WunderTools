79975f2f8fd8beaecac00c743e98f3fbc2eac57d
Agnis Mateuss <agnis.mateuss@wunder.io>
2018-05-30 18:35:35 +0300

    Vagrant environment and basics

--- drupal/web/sites/default/settings.php
+++ drupal/web/sites/default/settings.php
@@ -88,3 +88,4 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+$settings['install_profile'] = 'standard';
