3e50d05d4e2bac382c656574f7693ba9b3adbe52
Tormi Tabor <tormi.tabor@gmail.com>
2019-04-25 17:14:30 +0300

    Refactor settings.lando.php.

--- drupal/web/sites/default/settings.lando.php
+++ drupal/web/sites/default/settings.lando.php
@@ -1,29 +1,5 @@
 <?php
 
-$base_url = "https://wundertools.lndo.site";
-
-// Imagemagick GD binary seems to be unavailable in Lando
-$conf['imagemagick_gm'] = 0;
-$conf['imagemagick_convert'] = '/usr/bin/convert';
-
-// Set testing mode on locally
-$conf['simple_environment_indicator'] = 'DarkGreen Lando';
-
-$conf['stage_file_proxy_origin'] = 'https://www.wundertools.fi';
-$conf['stage_file_proxy_hotlink'] = TRUE;
-
-// SOLR.
-$conf['search_api_solr_overrides'] = array(
-  'solr' => array(
-    'name' => 'Solr Server (Overridden)',
-    'options' => array(
-      'host' => 'localhost',
-      'port' => 8983,
-      'path' => '/solr',
-    ),
-  ),
-);
-
 /*
  * Load database credentials from Lando.
  */
@@ -36,3 +12,54 @@ $databases['default']['default'] = [
   'host' => $lando_info['database']['internal_connection']['host'],
   'port' => $lando_info['database']['internal_connection']['port'],
 ];
+
+/**
+ * Generate the hash_salt from LANDO_HOST_IP.
+ */
+$settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
+
+/**
+ * Skip file system permissions hardening.
+ */
+$settings['skip_permissions_hardening'] = TRUE;
+
+/**
+ * Skip trusted host pattern.
+ */
+$settings['trusted_host_patterns'] = ['.*'];
+
+/**
+ * Base URL.
+ */
+$base_url = "https://wundertools.lndo.site";
+
+/**
+ * Simple Environment Indicator testing mode on.
+ */
+$conf['simple_environment_indicator'] = 'DarkGreen Lando';
+
+/**
+ * Stage File Proxy setup.
+ */
+$conf['stage_file_proxy_origin'] = '';
+$conf['stage_file_proxy_hotlink'] = TRUE;
+
+/**
+ * Imagemagick GD binary setup.
+ */
+// $conf['imagemagick_gm'] = 0;
+// $conf['imagemagick_convert'] = '/usr/bin/convert';
+
+/**
+ * SOLR setup.
+ */
+// $conf['search_api_solr_overrides'] = array(
+//   'solr' => array(
+//     'name' => 'Solr Server (Overridden)',
+//     'options' => array(
+//       'host' => 'localhost',
+//       'port' => 8983,
+//       'path' => '/solr',
+//     ),
+//   ),
+// );
