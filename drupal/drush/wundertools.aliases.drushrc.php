086b6cbd81e02342c41e7622b59f2f2a577b642a
Santeri Lindgren <santeri.lindgren@wunder.io>
2019-04-25 14:48:07 +0300

    Provide some new lando configurations and scripts.

--- drupal/drush/wundertools.aliases.drushrc.php
+++ drupal/drush/wundertools.aliases.drushrc.php
@@ -1,26 +1,4 @@
 <?php
-$home = drush_server_home();
-// Solve the key file to use
-$path = explode('/', dirname(__FILE__));
-array_pop($path);
-array_pop($path);
-$path[] = '.vagrant';
-$path = implode('/', $path);
-$key = shell_exec('find ' . $path . ' -iname private_key');
-if (!$key) {
-  $key = $home . '/.vagrant.d/insecure_private_key';
-}
-$key = rtrim($key);
-
-$aliases['local'] = array(
-  'parent' => '@parent',
-  'site' => 'ansibleref',
-  'env' => 'vagrant',
-  'root' => '/vagrant/drupal/current',
-  'remote-host' => 'local.wundertools.fi',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-i ' . $key,
-);
 
 $aliases['dev'] = array(
   'uri' => 'wundertools.dev.wunder.io',
